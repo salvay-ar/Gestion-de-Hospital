e84d86335b291436745426947bb0dfb33b335c7a
FarmaJava <facundomaldonado258@gmail.com>
2026-08-10 10:13:19 -0300

    holas buenas tardes, te dejo el css

--- Proyecto01/settings.py
+++ Proyecto01/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gestionhospital',
         'USER': 'root',
-        'PASSWORD': 'arianasalvay1',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
